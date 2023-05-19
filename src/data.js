d49a1e65da8be4c561acbfda83a60ebe08be3794
[jiansorge] <[ji@nsorge.com]>
2023-05-18 17:18:48 -0700

    update footer style and content

--- src/data.js
+++ src/data.js
@@ -132,8 +132,8 @@ export const footerData = {
     // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   // ],
-  // footNote: `
-  //   <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-  //   Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  // `,
+  footNote: `
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://astro.build/favicon.svg)] bg-primary"></span>
+    Built on <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://astro.build/"> Astro</a>.
+  `,
 };
